9a2f8421114b65fb1a2f1d109c95a1a200ec8cb1
Alan Lins <alanblins@gmail.com>
2022-09-24 17:51:08 +0100

    initial commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+import mymodules 
+#f = open('folder/small.txt', 'r')
+#text = f.read()
+#count = mymodules.PatternCount('asdfasdf','asdf')
+#mymodules.FrequentWords('ACTGACTCCCACCCC', 3)
+number = mymodules.PatternToNumber('AC')
+print(number)
+
+pattern = mymodules.NumberToPattern(1,2)
+print('Pattern')
+print(pattern)
---
+++ mymodules.py
@@ -0,0 +1,56 @@
+def PatternCount(text, pattern):
+    count = 0
+    for x in range(0,len(text)-len(pattern)+1) :
+        if Extract(text, x, pattern) == pattern :
+            count+=1
+    return count
+
+def Extract(text, position, pattern) :
+    return text[position:position+len(pattern)]
+
+
+def FrequentWords(text, k):
+    maxValue=0
+    word=''
+    for x in range(0, len(text) -k+1):
+        pattern = text[x:x+k+1]
+        count = PatternCount(text, pattern)
+        if  count > maxValue:
+            maxValue=count
+            word=pattern
+    print(word)
+    print(maxValue)
+
+def SymbolToNumber(letter):
+    return 'ACGT'.index(letter)
+
+def NumberToSymbol(n):
+    return 'ACGT'[n]
+
+def Prefix(word):
+    return word[:-1]
+
+def Last(word):
+    return word[-1]
+
+
+def PatternToNumber(word):
+    if word == '' :
+        return 0
+    return 4*PatternToNumber(Prefix(word))+SymbolToNumber(Last(word))
+
+
+def NumberToPattern(index, k):
+    if k == 1:
+        return NumberToSymbol(index)
+    prefixIndex = index // 4
+    r = index % 4
+    symbol = NumberToSymbol(r)
+    PrefixPattern = NumberToPattern(prefixIndex, k-1)
+    return PrefixPattern+symbol
+
+def ComputingFrequencies(text, k):
+    frequencyArray = [0]*(4**k-1)
+    for i in range(0, len(text)-k+1) :
+        pattern = text[i:i+k+1]
+    return ''
